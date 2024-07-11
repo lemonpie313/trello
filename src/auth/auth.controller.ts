7809b843ccfe9cbd8684aacd6600c492ae321feb
jeonsuwon <swjeon0330@gmail.com>
2024-07-11 22:08:34 +0900

    add: 로그인

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './Dto/sign-up.dto';
+import { SignInDto } from './Dto/sign-in.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -13,10 +14,20 @@ export class AuthController {
 
       return  {
         statuscode: HttpStatus.OK,
-        message: '정보조회에 성공하셧습니다.',
+        message: '회원가입에 성공하셧습니다.',
         data: {...data, password:undefined}
       }
    }
 
+  @Post('/sign-in')
+   async signIn(@Body() signInDto:SignInDto){
+    const data = await this.authService.signIn(signInDto);
+
+    return  {
+      statuscode: HttpStatus.OK,
+      message: '로그인에 성공하셧습니다.',
+      data
+    }
+   }
 }
   
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { SignUpDto } from './Dto/sign-up.dto';
 import bcrypt from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
+import { SignInDto } from './Dto/sign-in.dto';
 
 @Injectable()
 export class AuthService {
@@ -18,7 +19,6 @@ export class AuthService {
     if(password !== passwordConfirm) throw new BadRequestException('두 비밀번호가 일치하지 않습니다.')
     
     const existEmail = await this.userRepository.findOneBy({email})
-    console.log(existEmail);
     if(existEmail) throw new NotFoundException ('존재하는 이메일입니다.')
     
     const passwordHash = this.configService.get<string>('PASSWORD_HASH')
@@ -27,5 +27,18 @@ export class AuthService {
     const data = await this.userRepository.save({email, password:hashedPassword, bio})
 
     return data
+  }
+
+  async signIn({email, password}:SignInDto){
+    const user = await this.userRepository.findOneBy({email})
+    console.log(user)
+    if(!user) throw new NotFoundException ('존재하는 유저입니다.')
+
+    const comparedPassword = bcrypt.compareSync(password, user.password);
+    console.log(comparedPassword)
+    if(!comparedPassword) throw new BadRequestException('비밀번호가 일치하지 않습니다.')
+
+    return user
+    
   }
 }
