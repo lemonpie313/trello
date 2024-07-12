52bbc3b0f3196249dd02dc065f7785a72a9fcb83
jeonsuwon <swjeon0330@gmail.com>
2024-07-12 12:47:23 +0900

    add: 로그인, 자기정보조회

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,27 @@
-import { Controller, Get, HttpStatus } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, HttpStatus, Request, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { UsersService } from './users.service';
+import { AuthGuard } from '@nestjs/passport';
+import { JwtPayload } from 'src/auth/interface/jwt-payload.interface';
 
-@ApiTags('테스트')
+@ApiTags('USER API')
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService:UsersService){}
   /**
-   * 테스트용
+   * findme
    * @returns
    */
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
   @Get('/me')
-  async findMe() {
+  async findMe(@Request() req) {
+    const userId = req.id;
+    const data = this.usersService.findMe(userId)
     return {
         statusCode: HttpStatus.OK,
         message: '테스트 성공.',
+        data
       };
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './entity/users.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  @InjectRepository(User) private readonly userRepository: Repository<User>
+
+  findMe(id:number){
+    const user = this.userRepository.findOneBy({id})
+    return user
+  }
+}
