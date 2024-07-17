4c63a38d46002a9f7a404c9f103d28e4bc297016
yewon kim <yewonkim@yewonui-MacBookPro.local>
2024-07-17 12:44:40 +0900

    fix. 백엔드 서버 연결 경로 수정

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -24,7 +24,7 @@ import { configModuleValidationSchema } from './configs/env-validation.config';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
+        type: 'mysql',
         host: configService.get<string>('DB_HOST'),
         port: configService.get<number>('DB_PORT'),
         username: configService.get<string>('DB_USERNAME'),
